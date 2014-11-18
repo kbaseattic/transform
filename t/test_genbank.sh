750791a141d7f12b088ceb8a17a817ad72d9d85e
Shinjae Yoo <sjyoo@bnl.gov>
2014-11-18 20:05:35 +0000

    Updated GBK related script to support additional jar file and main execution class

--- t/test_genbank.sh
+++ t/test_genbank.sh
@@ -1,5 +1,7 @@
 java -cp $CP us.kbase.genbank.ValidateGBK /kb/dev_container/modules/transform/t/genbank/NC_005213
 java -cp $CP us.kbase.genbank.ValidateGBK /kb/dev_container/modules/transform/t/genbank/NC_009925
 
-java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_005213 loader_test http://localhost:7058 
-java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_009925 loader_test http://localhost:7058
+#java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_005213 loader_test http://localhost:7058 
+#java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_009925 loader_test http://localhost:7058
+java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_005213 
+java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_009925 

ffa2f3b4de88c42a847a33b35385c35eebf5e78b
Shinjae Yoo <sjyoo@bnl.gov>
2014-11-18 17:47:41 +0000

    Changed test script for GBK and GBK default test mode

--- t/test_genbank.sh
+++ t/test_genbank.sh
@@ -1,5 +1,5 @@
 java -cp $CP us.kbase.genbank.ValidateGBK /kb/dev_container/modules/transform/t/genbank/NC_005213
 java -cp $CP us.kbase.genbank.ValidateGBK /kb/dev_container/modules/transform/t/genbank/NC_009925
 
-java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_005213 KBaseTransformDev http://dev04.berkeley.kbase.us:7058 
-java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_009925 KBaseTransformDev http://dev04.berkeley.kbase.us:7058
+java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_005213 loader_test http://localhost:7058 
+java -cp $CP us.kbase.genbank.ConvertGBK /kb/dev_container/modules/transform/t/genbank/NC_009925 loader_test http://localhost:7058

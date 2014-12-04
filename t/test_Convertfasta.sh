c2905f069943a93181b94c2cbfad995272775e77
root <root@g2p1.novalocal>
2014-12-04 20:18:59 +0000

    test scripts modified

--- t/test_Convertfasta.sh
+++ t/test_Convertfasta.sh
@@ -1,3 +1,6 @@
 # Test Script for Converting to Kbase format for Fasta files
 
-/kb/dev_container/modules/transform/scripts/trns_transform_KBaseAssembly.FA-to-KBaseAssembly.ReferenceAssembly.py --in_id 5ee701d3-d238-4dff-9326-d0e256b11247 -f ./test-fasta/Sample1.fa -d "dummy_handleid"
+/kb/dev_container/modules/transform/scripts/trns_transform_KBaseAssembly.FA-to-KBaseAssembly.ReferenceAssembly.py -s http://10.1.16.87:7078 -n http://10.1.16.87:7109 -i d777a897-8630-4016-8e3a-87c6bc36e2cd -w testfa -f ./test-fasta/Sample1.fa -o fasta.json
+
+echo "Please check the files fasta.json ( workspace object type : KBaseAssembly.ReferenceAssembly )"
+echo "DONE"

b53e496e90e7ef8ab86bd3cf8d1360cc17377991
seaver <seaver@mcs.anl.gov>
2014-12-12 15:06:54 -0600

    Documentation added to Transcripts.FASTA - genome and SBML - FBAModel

--- scripts/trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl
+++ scripts/trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl
@@ -4,18 +4,45 @@ use strict;
 use Data::Dumper;
 use Getopt::Long;
 
+=head1 NAME
+
+trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl
+
+=head1 SYNOPSIS
+
+trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl --input sbml-file --output model-id --out_ws workspace-id [--genome genome-id]
+
+=head1 DESCRIPTION
+
+Transform an SBML file into a KBaseFBA.FBAModel object in the
+workspace. A genome object in the same workspace is needed if one
+wishes to explore gene-reaction associations.
+
+=head1 COMMAND-LINE OPTIONS
+trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl --input --output --out_ws [--genome]
+	-i --input      sbml file
+	-o --output     id under which KBaseFBA.FBAModel is to be saved
+        -w --out_ws     workspace where KBaseFBA.FBAModel is to be saved
+        -g --genome     id of KBaseGenomes.Genome object to associate with KBaseFBA.FBAModel. The object must be in the same workspace designated with out_ws
+	--help          print usage message and exit
+
+=cut
+
 my $In_File   = "";
 my $Out_Model = "";
 my $Out_WS    = "";
 my $Genome    = "Empty";
+my $Help      = 0;
 
-GetOptions("in_file|i=s"   => \$In_File,
-	   "out_model|o=s" => \$Out_Model,
-	   "out_ws|w=s"    => \$Out_WS,
-	   "genome|g=s"    => \$Genome);
+GetOptions("input|i=s"  => \$In_File,
+	   "output|o=s" => \$Out_Model,
+	   "out_ws|w=s" => \$Out_WS,
+	   "genome|g=s" => \$Genome,
+	   "help|h"     => \$Help);
 
-if(!$In_File || !$Out_Model || !$Out_WS){
-    die($0." --in_file|-i <Input Fasta File> --out_model|-o <Output FBAModel ID> --out_ws|-w <Workspace to save FBAModel in>");
+if($Help || !$In_File || !$Out_Model || !$Out_WS){
+    print($0." --input/-i <Input SBML File> --output/-o <Output FBAModel ID> --out_ws/-w <Workspace to save FBAModel in> --genome/-g <Input Genome ID>");
+    exit();
 }
 
 if(!-f $In_File){
@@ -45,3 +72,23 @@ $Genome_WS = "PlantSEED" if $Genome eq "Empty";
 my $Model = $In_File; $Model =~ s/\.sbml$/_Model/;
 
 $FBAImpl->import_fbamodel({workspace=>$Out_WS,genome=>$Genome,genome_workspace=>$Genome_WS,sbml=>$sbml,model=>$Model});
+
+__END__
+
+    Logging:
+
+Log output format
+           Datetime in UTC - name of script - log level:
+ log message
+<YYYY-MM-DDTHH:mm:SS>
+ - <script_name> - <level>: <message>
+
+           e.g., 2014-12-10T00:00:00Z - trns_transform_KBaseGenomes.GBK-to-KBaseGenomes.Genome
+ - INFO: Validating input file sample.gbff
+
+
+
+
+#1 How many <level> do you plan to support?
+#2 What?s the failure report LEVEL? FATAL, FAIL, ???
+#3 Do you have any recommended library to use to dump the above format?

d35a4b51e1dd363b6d64c3f2fa48cb45a1d89522
seaver <seaver@mcs.anl.gov>
2014-12-12 14:39:48 -0600

    Minor mods to SBML-FBA transform to include optional genome parameter

--- scripts/trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl
+++ scripts/trns_transform_KBaseFBA.SBML-to-KBaseFBA.FBAModel.pl
@@ -7,9 +7,12 @@ use Getopt::Long;
 my $In_File   = "";
 my $Out_Model = "";
 my $Out_WS    = "";
+my $Genome    = "Empty";
+
 GetOptions("in_file|i=s"   => \$In_File,
 	   "out_model|o=s" => \$Out_Model,
-	   "out_ws|w=s"    => \$Out_WS);
+	   "out_ws|w=s"    => \$Out_WS,
+	   "genome|g=s"    => \$Genome);
 
 if(!$In_File || !$Out_Model || !$Out_WS){
     die($0." --in_file|-i <Input Fasta File> --out_model|-o <Output FBAModel ID> --out_ws|-w <Workspace to save FBAModel in>");
@@ -19,8 +22,12 @@ if(!-f $In_File){
     die("Cannot find $In_File");
 }
 
-use Bio::KBase::fbaModelServices::ScriptHelpers qw( getToken );
-my $AToken = getToken();
+my $AToken = $ENV{KB_AUTH_TOKEN} ;
+
+if(!$AToken){
+    use Bio::KBase::fbaModelServices::ScriptHelpers qw( getToken );
+    $AToken = getToken();
+}
 
 use Bio::KBase::fbaModelServices::Impl;
 my $FBAImpl = Bio::KBase::fbaModelServices::Impl->new({'accounttype' => "kbase",
@@ -32,6 +39,9 @@ my $sbml;
 while(<FH>){$sbml.=$_;}
 close(FH);
 
+my $Genome_WS = $Out_WS;
+$Genome_WS = "PlantSEED" if $Genome eq "Empty";
+
 my $Model = $In_File; $Model =~ s/\.sbml$/_Model/;
 
-$FBAImpl->import_fbamodel({workspace=>$Out_WS,genome=>"Empty",genome_workspace=>"PlantSEED",sbml=>$sbml,model=>$Model});
+$FBAImpl->import_fbamodel({workspace=>$Out_WS,genome=>$Genome,genome_workspace=>$Genome_WS,sbml=>$sbml,model=>$Model});

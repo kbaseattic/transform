87fec955071092f728b945a03ee3adbc7280c376
Jason Baumohl <jkbaumohl@lbl.gov>
2015-01-16 17:39:48 -0800

    minor clean up and Matt did code review

--- scripts/trns_transform_KBaseGenomes_ContigSet_to_FASTA_DNA_Assembly.py
+++ scripts/trns_transform_KBaseGenomes_ContigSet_to_FASTA_DNA_Assembly.py
@@ -10,7 +10,7 @@ import json
 import logging
 import io
 import pprint
-import textwrap
+import string
 
 # 3rd party imports
 import requests
@@ -19,6 +19,12 @@ import requests
 import biokbase.Transform.script_utils as script_utils 
 import biokbase.workspace.client 
 
+
+def insert_newlines(string, every):
+    lines = []
+    for i in xrange(0, len(string), every):
+        lines.append(string[i:i+every])
+    return '\n'.join(lines)+"\n"
 
 # conversion method that can be called if this module is imported
 # Note the logger has different levels it could be run.  See: https://docs.python.org/2/library/logging.html#logging-levels
@@ -52,37 +58,55 @@ def convert(workspace_service_url, shock_service_url, handle_service_url, worksp
     try:
         ws_client = biokbase.workspace.client.Workspace(workspace_service_url) 
         if object_version_number and object_name :
-            contig_set = ws_client.get_objects([{'workspace':workspace_name,'name':object_name, 'ver':object_version_number}])[0]['data'] 
+            contig_set = ws_client.get_objects([{'workspace':workspace_name,'name':object_name, 'ver':object_version_number}])[0] 
         elif object_name:
-            contig_set = ws_client.get_objects([{'workspace':workspace_name,'name':object_name}])[0]['data'] 
+            contig_set = ws_client.get_objects([{'workspace':workspace_name,'name':object_name}])[0]
         elif object_version_number and object_id :
-            contig_set = ws_client.get_objects([{'workspace':workspace_name,'objid':object_id, 'ver':object_version_number}])[0]['data'] 
+            contig_set = ws_client.get_objects([{'workspace':workspace_name,'objid':object_id, 'ver':object_version_number}])[0]
         else:
-            contig_set = ws_client.get_objects([{'workspace':workspace_name,'objid':object_id}])[0]['data'] 
-    except: 
+            contig_set = ws_client.get_objects([{'workspace':workspace_name,'objid':object_id}])[0] 
+    except Exception, e: 
         logger.exception("Unable to rerieve ws object.")
         logger.exception("".join(traceback.format_exc()))
-        print "".join(traceback.format_exc()) 
-        sys.exit(1) 
+        raise 
 
     shock_id = None 
     if "fasta_ref" in contig_set : 
-        shock_id  = contig_set['fasta_ref'] 
+        shock_id  = contig_set['data']['fasta_ref'] 
         logger.info("Retrieving data from Shock.")
         script_utils.download_file_from_shock(logger, shock_service_url, shock_id, working_directory, token) 
         
     else: 
+        ws_object_name = contig_set['info'][1]
+        valid_chars = "-_.(){0}{1}"format(string.ascii_letters, string.digits)
+        output_file_name = ''
+        output_file_chars = list()
+        for character in ws_object_name:
+            if character in valid_chars:
+                output_file_chars.append(character)
+            else:
+                output_file_chars.append('_')
+        if len(output_file_chars) == 0:
+            output_file_name = "ContigSetFile"
+        else:
+            output_file_name = ''.join(output_file_chars)
+
         logger.warning("The ContigSet does not have a fasta_ref to shock.  The fasta file will be attempted to be built from contig sequences in the object.")
-        contig_list = contig_set['contigs']
-#KNOW DIRECTORY, DONT KNOW NAME TO WRITE FILE OUT TO
-        for contig in contig_list:
-            print ">{}".format(contig['name'])
-#do 80 characters per line or whole sequence?
+        
+        output_file =  os.path.join(working_directory,output_file_name)
+ 
+        outFile = open(output_file, 'w') 
+        for contig in contig_set['data']['contigs']:
+            outFile.write(">{}\n".format(contig['name']))
+            #do 80 nucleotides per line
             if contig['sequence'] != '':
-                print '\n'.join(textwrap.wrap(contig['sequence'], 80))
+                outFile.write(insert_new_lines(contig['sequence'],80))
             else:
                 logger.warning("The ContigSet does not have a fasta_ref to shock or sequences in the contigs. A fasta file can not be created.")
+                outFile.close()    
                 return
+        outFile.close()    
+
     logger.info("Conversion completed.")
 
 

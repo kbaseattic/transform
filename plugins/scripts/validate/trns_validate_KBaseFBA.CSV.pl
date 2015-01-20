3f96e4a59c1c97e1582f233e5ae40620923f2cd9
Matt Henderson <mlhenderson@Matthews-MacBook-Pro-2.local>
2015-01-19 16:13:07 -0800

    script movement

--- plugins/scripts/validate/trns_validate_KBaseFBA.CSV.pl
+++ plugins/scripts/validate/trns_validate_KBaseFBA.CSV.pl
@@ -4,6 +4,7 @@ use strict;
 use Data::Dumper;
 use Getopt::Long;
 use File::Stream;
+use Bio::KBase::Transform::ScriptHelpers qw( getStderrLogger );
 
 my $In_File   = "";
 my $Help = 0;
@@ -11,15 +12,18 @@ GetOptions("input|i=s"  => \$In_File,
 	   "help|h"     => \$Help);
 
 if($Help || !$In_File){
-    print($0." --input/-i <Input Excel File>");
+    print($0." --input/-i <Input CSV File>");
+    $logger->warn($0." --in_file|-i <Input CSV File>");
     exit(0);
 }
 
 if(!-f $In_File){
+    $logger->warn("Cannot find file ".$In_File);
     die("Cannot find $In_File");
 }
 
 if($In_File !~ /\.([ct]sv|txt)$/){
+    $logger->warn("$In_File does not have correct suffix (.txt or .csv or .tsv)");
     die("$In_File does not have correct suffix (.txt or .csv or .tsv)");
 }
 
@@ -35,6 +39,7 @@ if(scalar(keys %Headers) < 2){
     print join("\n", map {uc($_)} split(/\t/,$Header_Line)),"\n";
     %Headers = map { uc($_) => 1 } split(/\t/,$Header_Line);
     if(scalar(keys %Headers) < 2){
+	logger->warn("$In_File either does not use commas or tabs as a separator, or does not have enough columns");
 	die("$In_File either does not use commas or tabs as a separator, or does not have enough columns");
     }
 }
@@ -55,24 +60,8 @@ if(!exists($Headers{ID}) || !exists($Headers{NAME})){
 }
 
 if(!$reactions && !$compounds && !$media){
+    $logger->warn("$In_File does not contain the obligatory headers for either reactions (ID,DIRECTION,GPR,EQUATION) or compounds (ID,NAME,CHARGE,FORMULA) or media (ID, NAME)\n");
     die("$In_File does not contain the obligatory headers for either reactions (ID,DIRECTION,GPR,EQUATION) or compounds (ID,NAME,CHARGE,FORMULA) or media (ID, NAME)\n");
 }elsif($reactions && $compounds){
     #Possible problem with mixing column headers, but wouldn't affect ModelSEED code if proper arguments used
 }
-
-__END__
-
-    Logging:
-
-Log output format
-           Datetime in UTC - name of script - log level:
- log message
-<YYYY-MM-DDTHH:mm:SS>
- - <script_name> - <level>: <message>
-
-           e.g., 2014-12-10T00:00:00Z - trns_transform_KBaseGenomes.GBK-to-KBaseGenomes.Genome
- - INFO: Validating input file sample.gbff
-
-#1 How many <level> do you plan to support?
-#2 What?s the failure report LEVEL? FATAL, FAIL, ???
-#3 Do you have any recommended library to use to dump the above format?

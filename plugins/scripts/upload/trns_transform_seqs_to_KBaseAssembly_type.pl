a8a1d6c06d3cf9dfc1cdbbb97ef95ec37dbcdeca
Fangfang Xia <fangfang.xia@gmail.com>
2015-01-21 17:37:11 -0800

    add config types for assembly uploaders

--- plugins/scripts/upload/trns_transform_seqs_to_KBaseAssembly_type.pl
+++ plugins/scripts/upload/trns_transform_seqs_to_KBaseAssembly_type.pl
@@ -7,43 +7,42 @@ use JSON;
 
 use Bio::KBase::HandleService;
 
-$help and die $usage;
-
 my $usage = <<"End_of_Usage";
 
 Usage: $0 [ options ]
 
-Upload one or two FASTA/FASTQ files to the shock server. 
+Upload one or two FASTA/FASTQ files to the shock server.
 
 Options:
 
   -h, --help                          - print this help message and exit
-  -s, --shock  URL                    - shock service URL (D = https://kbase.us/services/shock-api)              
-  -n, --handle URL                    - handle service URL (D = https://kbase.us/services/handle_service)
-  -d, --dir    directory              - directory containing the files
-  -o, --out    json                   - output JSON file of KBaseAssembly.PairedEndLibrary type
-  -token       string                 - token string
-  -t, --type   output type            - output KBaseAssembly type (PairedEndLibrary, SingleEndLibrary, ReferenceAssembly)
+  -s, --shock_service_url  URL        - shock service URL (D = https://kbase.us/services/shock-api)
+  -n, --handle_service_url URL        - handle service URL (D = https://kbase.us/services/handle_service)
+  -d, --input_directory    dir        - directory containing the input files
+  -o, --output_file_name   json       - output JSON file of KBaseAssembly.PairedEndLibrary type
+  --token                  string     - token string
+  -t, --type               string     - output KBaseAssembly type (PairedEndLibrary, SingleEndLibrary, ReferenceAssembly)
 
 Options for PairedEndLibrary:
 
-  -f, --read   read_file              - one or two read files (FASTA, FASTQ, or compressed forms)
-  -insert      float                  - insert size mean
-  -stdev       float                  - insert size standard deviation
-  -outward                            - this flag is set if reads in the pair point outward
+  -f, --file   sequence_file          - one or two read files (FASTA, FASTQ, or compressed forms)
+  --insert     float                  - insert size mean
+  --stdev      float                  - insert size standard deviation
+  --outward                           - this flag is set if reads in the pair point outward
 
 Options for SingleEndLibrary:
 
-  -f, --read   read_file              - one read file (FASTA, FASTQ, or compressed forms)
+  -f, --file   read_file              - one read file (FASTA, FASTQ, or compressed forms)
 
 Options for ReferenceAssembly:
 
-  -f, --read   contig_set             - one FASTA file containing a reference set of contigs
+  -f, --file   contig_set             - one FASTA file containing a reference set of contigs
+  --refname    text                   - genome name of the reference contig set
 
 Examples:
 
-  $0 -t PairedEndLibrary -d stage/path -f read1.fq -f read2.fq -insert 300 -stdev 60 -o pe.reads.json  
-  $0 -t SingleEndLibrary -d stage/path -f read.fasta -o se.reads.json  
+  $0 -t PairedEndLibrary -d stage/path -f read1.fq -f read2.fq -insert 300 -stdev 60 -o pe.reads.json
+  $0 -t SingleEndLibrary -d stage/path -f read.fasta -o se.reads.json
   $0 -t ReferenceAssembly -d stage/path -f genome.fa -o ref.json
 
 End_of_Usage
@@ -51,51 +50,83 @@ End_of_Usage
 my ($help, $shock_url, $handle_url);
 my ($type, $dir, $token);
 my (@inputs, $output);
-my ($insert, $stdev, $outward);
+my ($insert, $stdev, $outward, $refname);
 
-my $rc = GetOptions("h|help"     => \$help,
-                    "s|shock=s"  => \$shock_url,
-                    "n|handle=s" => \$handle_url,
-                    "o|out=s"    => \$output,
-                    "d|dir=s"    => \$input_dir,
-                    "f|read=s"   => \@inputs,
-                    "t|type=s"   => \$type,
-                    "insert=f"   => \$insert,
-                    "stdev=f"    => \$stdev,
-                    "outward"    => \$outward);
+my $rc = GetOptions("h|help"                 => \$help,
+                    "s|shock_service_url=s"  => \$shock_url,
+                    "n|handle_service_url=s" => \$handle_url,
+                    "o|output_file_name=s"   => \$output,
+                    "d|input_directory=s"    => \$input_dir,
+                    "f|file=s"               => \@inputs,
+                    "t|type=s"               => \$type,
+                    "insert=f"               => \$insert,
+                    "stdev=f"                => \$stdev,
+                    "outward"                => \$outward,
+                    "refname"                => \$refname);
 
 $token      ||= $ENV{KB_AUTH_TOKEN};
 $shock_url  ||= 'https://kbase.us/services/shock-api';
 $handle_url ||= 'https://kbase.us/services/handle_service';
 $dir        ||= '.';
 
+$help and die $usage;
 $type && @inputs >= 1 && @inputs <= 2 or die $usage;
 
 my $shock = { url => $shock_url, token => $token };
 my $handle_service = Bio::KBase::HandleService->new($handle_url);
 
-my $obj = upload_pe_lib($dir, $inputs, $insert, $stdev, $outward);
+my $obj;
+
+if ($type eq 'PairedEndLibrary') {
+    $obj = upload_pe_lib($shock, $dir, \@inputs, $insert, $stdev, $outward);
+} elsif ($type eq 'SingleEndLibrary') {
+    $obj = upload_se_lib($shock, $dir, \@inputs);
+} elsif ($type eq 'ReferenceAssembly') {
+    $obj = upload_ref($shock, $dir, \@inputs, $refname);
+} else {
+    die "Unrecognized output type: $type\n";
+}
 
 print_output($output, encode_json($obj));
 
 sub upload_pe_lib {
-    my ($dir, $inputs, $insert, $stdev, $outward) = @_;
+    my ($shock, $dir, $inputs, $insert, $stdev, $outward) = @_;
 
     my $obj;
     $obj->{interleaved} = 1 if @$inputs == 1;
     $obj->{insert_size_mean} = $insert if $insert;
     $obj->{insert_size_std_dev} = $stdev if $stdev;
     $obj->{read_orientation_outward} = 1 if $outward;
-    
+
     my $i;
     for (@$inputs) {
         my $file = "$dir/$_";
         $obj->{'handle_'.++$i} = validate_seq_file($file);
     }
 
-    return upload_files_in_obj($obj);
+    return upload_files_in_obj($obj, $shock);
 }
 
+sub upload_se_lib {
+    my ($shock, $dir, $inputs) = @_;
+
+    my $obj;
+    my $file = join("/", $dir, $inputs->[0]);
+    $obj->{handle} = validate_seq_file($file);
+
+    return upload_files_in_obj($obj, $shock);
+}
+
+sub upload_ref {
+    my ($shock, $dir, $inputs, $refname) = @_;
+
+    my $obj;
+    my $file = join("/", $dir, $inputs->[0]);
+    $obj->{handle} = validate_seq_file($file);
+    $obj->{reference_name} = $refname if $refname;
+
+    return upload_files_in_obj($obj, $shock);
+}
 
 sub validate_seq_file {
     my ($file) = @_;
@@ -104,6 +135,7 @@ sub validate_seq_file {
     $name =~ s/\.(gz|gzip|bzip|bzip2|bz|bz2|zip)$//;
     $name =~ s/\.tar$//;
     $name =~ /\.(fasta|fastq|fa|fq|fna|bas\.h5|bax\.h5)$/ or die "Unrecognized file type: $file\n";
+    $file =~ s|.*/||;
     return { file_name => $file, type => '_handle_to_be_' };
 }
 
@@ -142,14 +174,18 @@ sub curl_post_file {
     my ($file, $shock) = @_;
     my $token = $shock->{token};
     my $url   = $shock->{url};
-    my $attr = q('{"filetype":"reads"}'); # should reference have a different type?
-    my $cmd  = 'curl --connect-timeout 10 -s -X POST -F attributes=@- -F upload=@'.$file." $url/node ";
-    $cmd    .= " -H 'Authorization: OAuth $token'";
-    my $out  = `echo $attr | $cmd` or die "Connection timeout uploading file to Shock: $file\n";
-    my $json = decode_json($out);
+    my $attr  = q('{"filetype":"reads"}'); # should reference have a different type?
+    my $cmd   = 'curl --connect-timeout 10 -s -X POST -F attributes=@- -F upload=@'.$file." $url/node ";
+    $cmd     .= " -H 'Authorization: OAuth $token'";
+    my $out   = `echo $attr | $cmd` or die "Connection timeout uploading file to Shock: $file\n";
+    my $json  = decode_json($out);
     $json->{status} == 200 or die "Error uploading file: $file\n".$json->{status}." ".$json->{error}->[0]."\n";
     return $json->{data}->{id};
 }
 
-
-
+sub print_output {
+    my ($fname, $text) = @_;
+    open(F, ">$fname") or die "Could not open $fname";
+    print F $text;
+    close(F);
+}

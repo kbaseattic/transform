57e9a6bd53dad8310197076631f9a3a1f7f96d80
Steven Silvester <steven.silvester@ieee.org>
2015-05-12 20:56:54 -0500

    Only install deps if not already installed

--- deps/pylib.sh
+++ deps/pylib.sh
@@ -1,2 +1,5 @@
 pip install requests_toolbelt filemagic ftputil
-pip install git+https://github.com/metabolite-atlas/metatlas
+python_metatlas_installed=`pip list | grep '^metatlas (' | wc -l`
+if [ "$python_metatlas_installed" == "0" ]; then
+    pip install git+https://github.com/metabolite-atlas/metatlas
+fi
--- deps/sudo_install_tables.sh
+++ deps/sudo_install_tables.sh
@@ -2,4 +2,9 @@
 sudo apt-get install libblas3gf
 sudo apt-get install liblapack3gf
 sudo apt-get install libhdf5-serial-dev
+python_tables_installed=`pip list | grep '^tables (' | wc -l`
+if [ "$python_tables_installed" > "0" ]; then
+    exit 0
+fi
+echo "Installing python tables"
 pip install tables

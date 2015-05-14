de8f35e27452379f84ae2b8b4072844bac12bb6d
Steven Silvester <steven.silvester@ieee.org>
2015-05-13 21:06:38 -0500

    Rename an arg and pass all args to transform function

--- plugins/scripts/upload/trns_transform_mzML_LCMS_to_MetaboliteAtlas2_MAFileInfo.py
+++ plugins/scripts/upload/trns_transform_mzML_LCMS_to_MetaboliteAtlas2_MAFileInfo.py
@@ -32,9 +32,9 @@ if sys.version.startswith('3'):
 def transform(shock_service_url=None, handle_service_url=None,
               output_file_name=None, input_directory=None,
               working_directory=None, shock_id=None, handle_id=None,
-              input_mapping=None, name=None, polarity=None, atlases=None,
-              group=None, inclusion_order=None, normalization_factor=None,
-              retention_correction=None,
+              input_mapping=None, mzml_file_name=None, polarity=None,
+              atlases=None, group=None, inclusion_order=None,
+              normalization_factor=None, retention_correction=None,
               level=logging.INFO, logger=None):
     """
     Converts mzML file to MetaboliteAtlas2_MAFileInfo json string.
@@ -59,7 +59,7 @@ def transform(shock_service_url=None, handle_service_url=None,
                        directory and look for your files.
         level: Logging level, defaults to logging.INFO.
         atlases: List of MetaboliteAtlas atlas IDs.
-        name: Name of the file, optional.  Defaults to the file name.
+        mzml_file_name: Name of the file, optional.  Defaults to the file name.
         polarity: Run polarity.
         group: Run group.
         inclusion_order: Run inclusion_order.
@@ -198,8 +198,8 @@ def main():
                         help=script_details["Args"]["atlases"],
                         action='store', type=str, nargs='?',
                         required=False)
-    parser.add_argument('--name',
-                        help=script_details["Args"]["name"],
+    parser.add_argument('--mzml_file_name',
+                        help=script_details["Args"]["mzml_file_name"],
                         action='store', type=str, required=False)
     parser.add_argument('--normalization_factor',
                         help=script_details["Args"]["normalization_factor"],
@@ -219,6 +219,13 @@ def main():
                   shock_id=args.shock_id,
                   handle_id=args.handle_id,
                   input_mapping=args.input_mapping,
+                  mzml_file_name=args.mzml_files,
+                  polarity=args.polarity,
+                  atlases=args.atlases,
+                  group=args.group,
+                  inclusion_order=args.inclusion_order,
+                  normalization_factor=args.normalization_factor,
+                  retention_correction=args.retention_correction,
                   logger=logger)
     except Exception as e:
         logger.exception(e)

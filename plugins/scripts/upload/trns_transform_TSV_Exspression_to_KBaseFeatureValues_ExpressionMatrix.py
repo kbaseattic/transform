240df534653c66eca8cc9d9750db967a9a5702c8
Roman Sutormin <rsutormin@lbl.gov>
2015-07-20 16:45:10 -0700

    Expression uploader python wrapper is connected to java library.

--- plugins/scripts/upload/trns_transform_TSV_Exspression_to_KBaseFeatureValues_ExpressionMatrix.py
+++ plugins/scripts/upload/trns_transform_TSV_Exspression_to_KBaseFeatureValues_ExpressionMatrix.py
@@ -22,7 +22,8 @@ if sys.version.startswith('3'):
 def transform(workspace_service_url=None, workspace_name=None,
               object_name=None, output_file_name=None, input_directory=None, 
               working_directory=None, input_mapping=None, format_type=None, 
-              genome_object_name=None, level=logging.INFO, logger=None):
+              genome_object_name=None, fill_missing_values=None, data_type=None, 
+              data_scale=None, level=logging.INFO, logger=None):
     """
     Converts Expression TSV file to json string of KBaseFeatureValues.ExpressionMatrix type.
 
@@ -44,6 +45,9 @@ def transform(workspace_service_url=None, workspace_name=None,
         format_type: Mannually defined type of TSV file format.
         genome_object_name: Optional reference to a Genome object that will be used.
                             for mapping feature IDs to.
+        fill_missing_values: Flag for filling in missing values in matrix (0-false, 1-true).
+        data_type: Data type (default value is 'log-ratio').
+        data_scale: Data scale (default value is '1.0').
 
     Returns:
         JSON files on disk that can be saved as a KBase workspace objects.
@@ -56,24 +60,59 @@ def transform(workspace_service_url=None, workspace_name=None,
         logger = script_utils.stderrlogger(__file__)
 
     logger.info("Starting conversion of Expression TSV to KBaseFeatureValues.ExpressionMatrix")
-    token = os.environ.get('KB_AUTH_TOKEN')
+    # token = os.environ.get('KB_AUTH_TOKEN')
 
     if not working_directory or not os.path.isdir(working_directory):
         raise Exception("The working directory {0} is not a valid directory!"
                         .format(working_directory))
 
-    if output_file_name is None:
-        output_file_name = 'expression_output.json'
+    classpath = ["$KB_TOP/lib/jars/kbase/feature_values/kbase-feature-values-0.1.jar",
+                 "$KB_TOP/lib/jars/kohsuke/args4j-2.0.21.jar",
+                 "$KB_TOP/lib/jars/kbase/common/kbase-common-0.0.10.jar",
+                 "$KB_TOP/lib/jars/jackson/jackson-annotations-2.2.3.jar",
+                 "$KB_TOP/lib/jars/jackson/jackson-core-2.2.3.jar",
+                 "$KB_TOP/lib/jars/jackson/jackson-databind-2.2.3.jar",
+                 "$KB_TOP/lib/jars/kbase/auth/kbase-auth-1398468950-3552bb2.jar",
+                 "$KB_TOP/lib/jars/kbase/workspace/WorkspaceClient-0.2.0.jar"]
+    
+    mc = "us.kbase.kbasefeaturevalues.transform.ExpressionUploader"
 
-    run_info = dict()
-    run_info['type'] = "log-ratio"
-    run_info['scale'] = "1.0"
-    run_info['data'] = {'row_ids': ["g1"], 'col_ids': ["c1"], 'values': [[0.5]]}
-    objectString = simplejson.dumps(run_info, sort_keys=True, indent=4)
+    argslist = ["--workspace_service_url {0}".format(workspace_service_url),
+                "--workspace_name {0}".format(workspace_name),
+                "--object_name {0}".format(object_name),
+                "--input_directory {0}".format(input_directory),
+                "--working_directory {0}".format(working_directory)]
+    if output_file_name:
+        argslist.append("--output_file_name {0}".format(output_file_name))
+    if input_mapping:
+        argslist.append("--input_mapping {0}".format(input_mapping))
+    if format_type:
+        argslist.append("--format_type {0}".format(format_type))
+    if genome_object_name:
+        argslist.append("--genome_object_name {0}".format(genome_object_name))
+    if fill_missing_values:
+        logger.info("--fill_missing_values {0}".format(fill_missing_values))
+        argslist.append("--fill_missing_values")
+    if data_type:
+        argslist.append("--data_type {0}".format(data_type))
+    if data_scale:
+        argslist.append("--data_scale {0}".format(data_scale))
 
-    output_file_path = os.path.join(working_directory, output_file_name)
-    with open(output_file_path, "w") as outFile:
-        outFile.write(objectString)
+    arguments = ["java", "-classpath", ":".join(classpath), mc, " ".join(argslist)]
+
+    logger.debug(arguments)
+
+    # need shell in this case because the java code is depending on finding the KBase token in the environment
+    tool_process = subprocess.Popen(" ".join(arguments), stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = tool_process.communicate()
+
+    if stdout is not None and len(stdout) > 0:
+        logger.info(stdout)
+
+    if stderr is not None and len(stderr) > 0:
+        logger.error("Transformation from TSV.Expression to KBaseFeatureValues.ExpressionMatrix failed on {0}".format(input_directory))
+        logger.error(stderr)
+        sys.exit(1)
 
     logger.info("Conversion completed.")
 
@@ -118,6 +157,15 @@ def main():
     parser.add_argument('--genome_object_name',
                         help=script_details["Args"]["genome_object_name"],
                         action='store', type=str, required=False)
+    parser.add_argument('--fill_missing_values',
+                        help=script_details["Args"]["fill_missing_values"],
+                        action='store', type=int, required=False)
+    parser.add_argument('--data_type',
+                        help=script_details["Args"]["data_type"],
+                        action='store', type=str, required=False)
+    parser.add_argument('--data_scale',
+                        help=script_details["Args"]["data_scale"],
+                        action='store', type=str, required=False)
 
     args, unknown = parser.parse_known_args()
 
@@ -134,6 +182,9 @@ def main():
                   input_mapping=args.input_mapping,
                   format_type=args.format_type,
                   genome_object_name=args.genome_object_name,
+                  fill_missing_values=args.fill_missing_values,
+                  data_type=args.data_type,
+                  data_scale=args.data_scale,
                   logger=logger)
     except Exception as e:
         logger.exception(e)

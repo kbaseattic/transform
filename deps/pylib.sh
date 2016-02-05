dc9e895a9883aafb67a0946f8429896ec40bcf6a
Matt Henderson <mhenderson@lbl.gov>
2016-02-05 12:32:55 -0800

    Revert "Add upload script for mzML files"

--- deps/pylib.sh
+++ deps/pylib.sh
@@ -1,5 +1,1 @@
 pip install requests_toolbelt filemagic ftputil
-python_metatlas_installed=`pip list | grep '^metatlas (' | wc -l`
-if [ "$python_metatlas_installed" == "0" ]; then
-    pip install git+https://github.com/metabolite-atlas/metatlas
-fi

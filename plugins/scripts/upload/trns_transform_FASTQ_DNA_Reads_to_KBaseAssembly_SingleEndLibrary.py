acfae9ab1e4e2bfb20ff0784eaa41c43a45b01c0
Sean R. McCorkle <mccorkle@bnl.gov>
2016-06-20 22:52:34 -0400

    SRA conversion enhancement to trns_transform_FASTQ_DNA_Reads_to_KBaseAssembly_PairedEndLibrary.py

--- plugins/scripts/upload/trns_transform_FASTQ_DNA_Reads_to_KBaseAssembly_SingleEndLibrary.py
+++ plugins/scripts/upload/trns_transform_FASTQ_DNA_Reads_to_KBaseAssembly_SingleEndLibrary.py
@@ -12,12 +12,13 @@ import simplejson
 
 # KBase imports
 import biokbase.Transform.script_utils as script_utils
-
+import subprocess
+import pprint
 
 # transformation method that can be called if this module is imported
 def transform(shock_service_url=None, handle_service_url=None, 
               output_file_name=None, input_directory=None, 
-              working_directory=None, level=logging.INFO, logger=None):
+              working_directory=None, level=logging.INFO, sra = 0, logger=None):
     """
     Converts a FASTQ file to a KBaseAssembly.SingleEndLibrary json string.  
 
@@ -27,6 +28,7 @@ def transform(shock_service_url=None, handle_service_url=None,
         output_file_name: A file name where the output JSON string should be stored.  
         input_directory: The directory containing the file.
         working_directory: The directory the resulting json file will be written to.
+        input_mapping: a mysterious thing that Sean McCorkle had to insert
         level: Logging level, defaults to logging.INFO.
         
     Returns:
@@ -38,6 +40,26 @@ def transform(shock_service_url=None, handle_service_url=None,
     if logger is None:
         logger = script_utils.stderrlogger(__file__)
 
+    if  sra > 0 :
+        if  not os.path.isfile( "fastq-dump" ) :
+            logger.exception( "did not find fastq-dump in working directory" )
+        if  not os.access( "fastq-dump", os.X_OK ) :
+            logger.exception( "fastq-dump is not executable" )
+
+        logger.info( "scanning for SRA files" )
+
+        files = os.listdir(working_directory)
+        sra_files = [x for x in files if os.path.splitext(x)[-1] in [".sra"]]
+
+        if  len( sra_files ) < 0 :
+            logger.execption( "no sra files found in working directory" )
+
+        if  len( sra_files ) > 1 :
+            logger.exception( "several sra files found in working directory, exiting" )
+
+        logger.info( "converting " + sra_files[0] + " to fastq" )
+        subprocess.call( [ "./fastq-dump", sra_files[0] ] )
+    
     logger.info("Scanning for FASTQ files.")
 
     valid_extensions = [".fq",".fastq",".fnq"]
@@ -47,9 +69,12 @@ def transform(shock_service_url=None, handle_service_url=None,
         
     assert len(fastq_files) != 0
 
+    print("Found {0}".format(str(fastq_files)))
+
     logger.info("Found {0}".format(str(fastq_files)))
 
-    input_file_name = files[0]
+    #input_file_name = files[0]
+    input_file_name = fastq_files[0]
 
     if len(fastq_files) > 1:
         logger.warning("Not sure how to handle multiple FASTQ files in this context. Using {0}".format(input_file_name))
@@ -57,15 +82,18 @@ def transform(shock_service_url=None, handle_service_url=None,
 
     kb_token = os.environ.get('KB_AUTH_TOKEN')
 
-    script_utils.upload_file_to_shock(logger=logger,
-                                      shock_service_url=shock_service_url,
-                                      filePath=os.path.join(input_directory, input_file_name),
-                                      token=kb_token)
+    shock_res = script_utils.upload_file_to_shock(
+                                      logger = logger,
+                                      shock_service_url = shock_service_url,
+                                      filePath =os.path.join( input_directory, input_file_name ),
+                                      token = kb_token )
 
-    handles = script_utils.getHandles(logger=logger, 
-                                      shock_service_url=shock_service_url, 
-                                      handle_service_url=handle_service_url, 
-                                      token=kb_token)
+    handles = script_utils.getHandles( logger = logger, 
+                                       shock_service_url = shock_service_url, 
+                                       handle_service_url = handle_service_url, 
+                                       shock_ids = [ shock_res["id"] ],
+                                       token = kb_token)
+
 
     assert len(handles) != 0
 
@@ -74,6 +102,7 @@ def transform(shock_service_url=None, handle_service_url=None,
     if output_file_name is None:
         output_file_name = input_file_name
 
+    output_directory = ""
     with open(os.path.join(output_directory,output_file_name), "w") as f:
         f.write(objectString)
 
@@ -108,8 +137,14 @@ if __name__ == "__main__":
     parser.add_argument('--input_mapping', 
                         help=script_details["Args"]["input_mapping"], 
                         action='store', type=unicode, nargs='?', default=None, required=False)
+    parser.add_argument('--sra', 
+                        help='convert SRA to FASTQ beforehand', 
+                        action='store', 
+                        type=int, 
+                        default=0)
 
     args, unknown = parser.parse_known_args()
+
 
     logger = script_utils.stderrlogger(__file__)
     try:
@@ -118,8 +153,9 @@ if __name__ == "__main__":
                   output_file_name = args.output_file_name, 
                   input_directory = args.input_directory, 
                   working_directory = args.working_directory, 
-                  shock_id = args.shock_id, 
-                  handle_id = args.handle_id,
+                  #shock_id = args.shock_id, 
+                  #handle_id = args.handle_id,
+                  sra = args.sra,
                   logger = logger)
     except Exception, e:
         logger.exception(e)
